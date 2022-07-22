- bug fixes - start class_marginal_prediction_set.py - per-image per-class sis computation

run_sis_on_cifar.py
@@ -28,6 +28,7 @@ import os
 import numpy as np
 import torch
 from torchvision import datasets
+from torchvision.models import resnet18
 from tqdm import tqdm
 
 import additional_datasets
@@ -35,8 +36,11 @@ import inference_util
 import sis_util
 import util.misc_util as misc_util
 import util.data_util as data_util
+from config import DEVICE
 from sufficient_input_subsets import sis
 
+# import os
+# os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 
 DATASET_OPTIONS = [
     'cifar10_train',
@@ -65,12 +69,14 @@ args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 print(args)
 
-device = torch.device('cuda' if args.cuda else 'cpu')
+# device = torch.device('cuda' if args.cuda else 'cpu')
+device = DEVICE
 print('Using device: ', device)
 print()
 
 # Load model from checkpoint.
 model = inference_util.load_saved_model(args.saved_model_dir)
+# model = resnet18()
 model.to(device)
 model.eval()
 print('Loaded model')

sis_util.py
@@ -4,8 +4,8 @@ import numpy as np
 import torch
 
 import inference_util
+from config import DEVICE
 from sufficient_input_subsets import sis
-
 
 def make_f_for_class(model, class_idx, batch_size=128, add_softmax=False):
     def f(inputs):
@@ -13,9 +13,9 @@ def make_f_for_class(model, class_idx, batch_size=128, add_softmax=False):
             ret_np = False
             if isinstance(inputs, np.ndarray):
                 ret_np = True
-                inputs = torch.from_numpy(inputs).cuda()
+                inputs = torch.from_numpy(inputs).to(DEVICE)
             else:
-                inputs = inputs.cuda()
+                inputs = inputs.to(DEVICE)
             num_batches = int(np.ceil(inputs.shape[0] / batch_size))
             all_preds = []
             for batch_idx in range(num_batches):
@@ -40,9 +40,9 @@ def find_sis_on_input(model, x, initial_mask, fully_masked_input, threshold,
                       batch_size=128, add_softmax=False):
     """Find first SIS on input x with PyTorch model."""
     if isinstance(x, np.ndarray):
-        x = torch.from_numpy(x).cuda()
+        x = torch.from_numpy(x).to(DEVICE)
     with torch.no_grad():
-        pred = model(x.unsqueeze(0).cuda())[0]
+        pred = model(x.unsqueeze(0).to(DEVICE))[0]
         pred_class = int(pred.argmax())
         pred_confidence = float(pred.max())
     if pred_confidence < threshold: