aaa94790d5ef074a55432b96e5c35729fdf7f6f8
amitshmidov <shmidov.amit@gmail.com>
2022-07-30 12:39:31 +0300

    - per-image per-class sis - sis extraction from files

--- conformal_prediction/class_marginal_prediction_set.py
+++ conformal_prediction/class_marginal_prediction_set.py
@@ -1,11 +1,51 @@
 import numpy as np
+import os
+import torch
+from torchvision import datasets
+
+from util import data_util
 
 
-def compute_quantiles(X, Y, alpha, tau):
-    n = X.shape[0]
-    sis_tau = None
-    Q = np.sort(sis_tau)[np.floor(alpha * (n + 1))]  # vector of Q_y-s for each y value
+def get_image_from_sis_file_name(sis_file_name):
+    image_i = sis_file_name.split("__")[1].split("_")[1]
+    return int(image_i)
 
 
-def compute_prediction_set(x_test, Y, tau, alpha):
-    Q = compute_quantile(X, Y, alpha, tau)
+def get_class_from_sis_file_name(sis_file_name):
+    class_i = sis_file_name.split("__")[2].split("_")[1].split(".")[0]
+    return int(class_i)
+
+
+def load_sis(experiment_name):
+    sis_files = [f for f in os.listdir(f"../saved_models/{experiment_name}/") if "npz" in f]
+    images_idx = [get_image_from_sis_file_name(file_name) for file_name in sis_files]
+    classes_idx = [get_class_from_sis_file_name(file_name) for file_name in sis_files]
+    sis = np.zeros((np.max(images_idx) + 1, np.max(classes_idx) + 1))
+
+    for image_i in images_idx:
+        for class_i in classes_idx:
+            file_name = f"cifar10_test__image_{image_i}__class_{class_i}.npz"
+            full_file_name = f"../saved_models/{experiment_name}/{file_name}"
+            if file_name in sis_files:
+                sis[image_i, class_i] = np.load(full_file_name)["mask"].sum()
+            else:
+                sis[image_i, class_i] = 0
+    return sis
+
+
+# def compute_quantiles(X, Y, alpha, tau, experiment_name):
+#     n = X.shape[0]
+#     sis_tau = load_sis(experiment_name)
+#     Q = np.sort(sis_tau, axis=0)[np.floor(alpha * (n + 1))]  # vector of Q_y-s for each y value
+#
+#
+# def compute_prediction_set(x_test, Y, tau, alpha, experiment_name):
+#     Q = compute_quantiles(X, Y, alpha, tau, experiment_name)
+
+
+# if __name__ == '__main__':
+    # transform = data_util.cifar_test_transform()
+    # dataset = datasets.CIFAR10(root='data/',
+    #                            train=False,
+    #                            transform=transform,
+    #                            download=True)
--- sufficient_input_subsets/y_dependent_sis.py
+++ sufficient_input_subsets/y_dependent_sis.py
@@ -13,14 +13,19 @@ from sufficient_input_subsets import sis
 import numpy as np
 
 
+TOP_CLASSES_NUM = 5
+
+
 def single_image_single_y_sis(model, image, pred_class, pred_confidence, sis_threshold):
     initial_mask = sis.make_empty_boolean_mask_broadcast_over_axis([3, 32, 32], 0)
     fully_masked_image = np.zeros((3, 32, 32), dtype='float32')
 
     pred_class = int(pred_class)
     pred_confidence = float(pred_confidence)
-    if pred_confidence < sis_threshold:
-        return None
+    # if pred_confidence < sis_threshold:
+    #     return None
+    sis_threshold *= pred_confidence
+
     f_class = sis_util.make_f_for_class(model, pred_class, batch_size=128, add_softmax=True)
     sis_result = sis.find_sis(
         f_class,
@@ -38,9 +43,11 @@ def compute_sis_on_all_classes(model, dataset, sis_threshold, start_idx, end_idx
         if isinstance(image, np.ndarray):
             image = torch.from_numpy(image).to(DEVICE)
         with torch.no_grad():
-            pred = model(image.unsqueeze(0).to(DEVICE))[0]
+            preds = torch.sort(torch.softmax(model(image.unsqueeze(0).to(DEVICE))[0], 0).to("cpu"))
+            preds_confidences = preds[0][-TOP_CLASSES_NUM:].to(DEVICE)
+            preds_classes = preds[1][-TOP_CLASSES_NUM:].to(DEVICE)
 
-        for pred_class, pred_confidence in enumerate(pred):
+        for pred_class, pred_confidence in tqdm(list(zip(preds_classes, preds_confidences)), leave=False):
             sis_outfile = os.path.join(sis_out_dir, f"cifar10_test__image_{i}__class_{pred_class}.npz")
             if os.path.exists(sis_outfile):
                 continue  # File already exists.
@@ -57,12 +64,11 @@ if __name__ == '__main__':
                                train=False,
                                transform=transform,
                                download=True)
-    image, label = dataset[0]
-    model = inference_util.load_saved_model("../saved_models/resnet18_rep1/")
+    sis_out_dir = "../saved_models/resnet18_single_epoch"
+    model = inference_util.load_saved_model(sis_out_dir + "/")
     model.to(DEVICE)
     model.eval()
 
-    start_idx, end_idx = 0, 5
-    sis_out_dir = "../saved_models/resnet18_rep1"
+    start_idx, end_idx = 0, 100
 
     compute_sis_on_all_classes(model, dataset, sis_threshold, start_idx, end_idx, sis_out_dir)
